66ad698e38e7014bb65d33fb9a633a86e8b39762
KSlashh <48985735+KSlashh@users.noreply.github.com>
2026-02-02 12:11:27 +0800

    fix: conversion overflows

--- src/libraries/Converter.sol
+++ src/libraries/Converter.sol
@@ -9,7 +9,7 @@ library Converter {
     function _amountFromSats(uint64 amountSats) internal pure returns (uint256) {
         uint8 TokenDecimals = Constants.TokenDecimals;
         if (TokenDecimals >= BtcDecimals) {
-            return uint256(amountSats * uint64(10 ** (TokenDecimals - BtcDecimals)));
+            return uint256(amountSats) * 10 ** (TokenDecimals - BtcDecimals);
         } else {
             return uint256(amountSats / uint64(10 ** (BtcDecimals - TokenDecimals)));
         }
